4a09a46765bb8c4d3ae415b679aca9f65105ec84
zmflqhaos <zmflqhaos123@gmail.com>
2023-02-08 15:23:37 +0900

    데모플레이용 맵 제작

--- Assets/Scripts/Common/Ar/KamenRider.cs
+++ Assets/Scripts/Common/Ar/KamenRider.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class KamenRider : Boss
 {
+    [SerializeField] GameObject itemBox;
     protected override void Start()
     {
         base.Start();
         MaxHP = 1500;
         HP = MaxHP;
         ATK = 50;
+        itemBox.SetActive(false);
+        OnBattleDie.AddListener(SummonItemBox);
+        OnOutDie.AddListener(SummonItemBox);
+    }
+
+    private void SummonItemBox()
+    {
+        itemBox.SetActive(true);
     }
 }
