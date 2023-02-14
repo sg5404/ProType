d4a9fc201626b1d3ca6edc4a1e586658efbb0035
rlagmltn <gmltn2015@naver.com>
2023-02-14 12:07:59 +0900

    보기 쉽게 변경

--- Assets/Scripts/Common/Ar/Bullet.cs
+++ Assets/Scripts/Common/Ar/Bullet.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Bullet : Ar
 {
     public BulletSO bulletSO;
+
+    private void Start()
+    {
+        base.Start();
+        Destroy(this.gameObject, 10f);
+    }
+
+
 }
